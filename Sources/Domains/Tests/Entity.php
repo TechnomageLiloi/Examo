454e0077e20d9c277e497e372e342705e09b4eab
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-05-20 17:30:31 +0300

    [0.00.005] Add tests domain.

---
+++ Sources/Domains/Tests/Entity.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Liloi\Examo\Domains\Tests;
+
+use Liloi\Tools\Entity as AbstractEntity;
+use Liloi\Stylo\Parser as StyloParser;
+
+/**
+ * @method string getTitle()
+ * @method void setTitle(string $value)
+ *
+ * @method string getProgram()
+ * @method void setProgram(string $value)
+ *
+ * @method string getStatus()
+ * @method void setStatus(string $value)
+ *
+ * @method string getType()
+ * @method void setType(string $value)
+ *
+ * @method string getDone()
+ * @method void setDone(string $value)
+ *
+ * @method string getHint()
+ * @method void setHint(string $value)
+ *
+ * @method string getNode()
+ * @method void setNode(string $value)
+ */
+class Entity extends AbstractEntity
+{
+    public function getKey(): string
+    {
+        return $this->getField('key_test');
+    }
+
+    public function getKeySuite(): string
+    {
+        return $this->getField('key_suite');
+    }
+
+    public function setKeySuite(string $value): string
+    {
+        $this->setField('key_suite', $value);
+    }
+
+    public function getProgramParse(): string
+    {
+        return StyloParser::parseString($this->getProgram());
+    }
+
+    public function save(): void
+    {
+        Manager::save($this);
+    }
+}
---
+++ Sources/Domains/Tests/Manager.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Liloi\Examo\Domains\Tests;
+
+use Liloi\TARDIS\Domains\Manager as DomainManager;
+use Liloi\TARDIS\Application;
+
+class Manager extends DomainManager
+{
+    /**
+     * Get table name.
+     *
+     * @return string
+     */
+    public static function getTableName(): string
+    {
+        return self::getTablePrefix() . 'tests';
+    }
+
+    public static function loadCollection(): Collection
+    {
+        $name = self::getTableName();
+
+        $rows = self::getAdapter()->getArray(sprintf(
+            'select * from %s where key_test order by key_test desc;',
+            $name
+        ));
+
+        $collection = new Collection();
+
+        foreach($rows as $row)
+        {
+            $collection[$row['key_test']] = Entity::create($row);
+        }
+
+        return $collection;
+    }
+
+    /**
+     * Load problem from database.
+     *
+     * @param string $keyQuest
+     * @return Entity
+     */
+    public static function load(string $keyQuest): Entity
+    {
+        $name = self::getTableName();
+
+        $row = self::getAdapter()->getRow(sprintf(
+            'select * from %s where key_test="%s"',
+            $name,
+            $keyQuest
+        ));
+
+        return Entity::create($row);
+    }
+
+    /**
+     * Save problem to database.
+     *
+     * @param Entity $entity
+     */
+    public static function save(Entity $entity): void
+    {
+        $name = self::getTableName();
+        $data = $entity->get();
+
+        $keyQuest = $data['key_test'];
+
+        self::update(
+            $name,
+            $data,
+            sprintf('key_test = "%s"', $keyQuest)
+        );
+    }
+
+    /**
+     * Create problem in database.
+     */
+    public static function create(string $keyTest, string $keySuite): Entity
+    {
+        $name = self::getTableName();
+        $data = [
+            'key_test' => $keyTest,
+            'key_suite' => $keySuite,
+            'title' => '-',
+            'program' => '-',
+            'hint' => '-',
+            'note' => '-'
+        ];
+
+        self::insert($name, $data);
+        return Entity::create($data);
+    }
+}
