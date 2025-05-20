19cc24e7c0973be049b26b104552fd400ee1a2bf
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-05-20 14:03:20 +0300

    [0.00.002] Add sutes and tests database tables.

--- Install/Database.sql
+++ Install/Database.sql
@@ -15,3 +15,32 @@ create table examo_config
     constraint examo_config_pk
         primary key (key_config)
 );
+
+create table examo_suites
+(
+	key_suite varchar(1000) not null,
+	title varchar(100) not null,
+	program text not null,
+	mark tinyint unsigned default 0 not null,
+	data json not null,
+	constraint examo_suites_pk
+		primary key (key_suite)
+);
+
+create table examo_tests
+(
+	key_test varchar(100) not null,
+	key_suite varchar(1000) not null,
+	title varchar(100) not null,
+	program json not null,
+	status tinyint unsigned default 1 not null,
+	type tinyint unsigned default 1 not null,
+	done tinyint unsigned default 0 not null,
+	hint text not null,
+	constraint examo_tests_pk
+		primary key (key_suite, key_test),
+    constraint examo_tests_key_suite_fk
+        foreign key (key_suite) references examo_suites (key_suite)
+            on update cascade on delete cascade
+);
+
