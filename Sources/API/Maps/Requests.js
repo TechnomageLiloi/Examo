da9d0231963656e8a95ec31714b87eaf06774302
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-05-24 18:23:53 +0300

    [0.02.005] Add URL ID check.

--- Sources/API/Maps/Requests.js
+++ Sources/API/Maps/Requests.js
@@ -4,6 +4,14 @@ Requests.Maps = {
         API.request('Maps.Show', {
             debug: false
         }, function (data) {
+
+            let url = window.location.href;
+            if(url.indexOf('#') >= 0)
+            {
+                alert(url);
+                return;
+            }
+
             $('#page').html(data.render);
         }, function () {
 
